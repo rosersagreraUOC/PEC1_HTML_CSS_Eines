3d6fc3cde996d4677e0fb431b334ad8ba78d0b55
rosersagrera <rosersagrera15@gmail.com>
2024-05-26 19:34:34 +0200

    Initial commit1

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,14 @@
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    const images = document.querySelectorAll('.carousel img');
-    let index = 0;
+// document.addEventListener('DOMContentLoaded', function() {
+//     const images = document.querySelectorAll('.carousel img');
+//     let index = 0;
 
-    function changeImage() {
-        images[index].classList.remove('active');
-        index = (index + 1) % images.length;
-        images[index].classList.add('active');
-    }
+//     function changeImage() {
+//         images[index].classList.remove('active');
+//         index = (index + 1) % images.length;
+//         images[index].classList.add('active');
+//     }
 
-    setInterval(changeImage, 3000);
-});
+//     setInterval(changeImage, 3000);
+// });
